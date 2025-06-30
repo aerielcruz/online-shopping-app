57b55914f0ac4d406aac473d4675b5cf5380033a
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-06-30 21:41:03 +1200

    Update get user from session

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -2,22 +2,24 @@ import { createContext, useState, useEffect } from 'react';
 
 export const UserContext = createContext();
 
-const getInitialState = () => {
+const getUserFromSession = () => {
   const user = sessionStorage.getItem("user");
-  if (user) {
-    const parsedUser = JSON.parse(user)
-    if (Object.keys(parsedUser).length === 0) {
-      return parsedUser
-    }
-  }
-  return null
+  if (!user) return null;
+
+  const parsedUser = JSON.parse(user);
+  return Object.keys(parsedUser).length > 0 ? parsedUser : null;
 }
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(getInitialState);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    sessionStorage.setItem("user", JSON.stringify(user))
+    if (user) {
+      sessionStorage.setItem("user", JSON.stringify(user))
+    } else {
+      const user = getUserFromSession()
+      setUser(user)
+    }
   }, [user])
 
   return (
