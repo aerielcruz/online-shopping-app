fd104138becb1bca550a88ef44b433704f947d37
Aeriel Matthew Cruz <aerielmatthew@gmail.com>
2025-07-04 12:21:44 +1200

    Update apiUrl

--- client/src/client/index.js
+++ client/src/client/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = 'http://doodleverse-c8cdf6cybmcua3fv.australiaeast-01.azurewebsites.net'
+const apiUrl = 'doodleverse-c8cdf6cybmcua3fv.australiaeast-01.azurewebsites.net'
 
 export const client = axios.create({
   baseURL: (apiUrl || 'http://localhost:3000') + '/api/v1',
